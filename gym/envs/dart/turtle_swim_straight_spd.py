330229b23fea535a8dc2f6017d32754c1855b35a
DragonMyth <tiger961212@gmail.com>
2017-10-10 21:24:58 -0400

    Add no enforcement env

--- gym/envs/dart/turtle_swim_straight_spd.py
+++ gym/envs/dart/turtle_swim_straight_spd.py
@@ -25,7 +25,8 @@ class DartTurtleSwimStraighSPDEnv(dart_env.DartEnv, utils.EzPickle):
         self.Kd = self.simulation_dt * self.Kp
 
         self.invM = np.linalg.inv(self.robot_skeleton.M + self.Kd * self.simulation_dt)
-        self.symm_rate  = 0#-0.3*np.array([1,1,0.1,0.1])
+        self.symm_rate = -1 * np.array([1, 1, 0.1, 0.1])
+
     def _step(self, a):
         old_com = self.robot_skeleton.C
         old_q = self.robot_skeleton.q
@@ -50,7 +51,7 @@ class DartTurtleSwimStraighSPDEnv(dart_env.DartEnv, utils.EzPickle):
         tau = p + d - self.Kd.dot(qddot) * self.simulation_dt
         tau *= 0.0005
         tau[0:len(self.robot_skeleton.joints[0].dofs)] = 0
-        self.do_simulation(tau,self.frame_skip)
+        self.do_simulation(tau, self.frame_skip)
         cur_com = self.robot_skeleton.C
         cur_q = self.robot_skeleton.q
         cur_dq = self.robot_skeleton.dq
@@ -61,21 +62,21 @@ class DartTurtleSwimStraighSPDEnv(dart_env.DartEnv, utils.EzPickle):
         torque_cost_rate = 0.001
         symm_pos_pen = np.sum(self.symm_rate *
                               np.abs(cur_q['left_front_limb_joint_1',
-                                            'left_front_limb_joint_2',
+                                           'left_front_limb_joint_2',
                                            'left_rear_limb_joint_1',
-                                           'left_rear_limb_joint_2']-
-                                    cur_q['right_front_limb_joint_1',
-                                          'right_front_limb_joint_2',
-                                          'right_rear_limb_joint_1',
-                                          'right_rear_limb_joint_2']))
+                                           'left_rear_limb_joint_2'] -
+                                     cur_q['right_front_limb_joint_1',
+                                           'right_front_limb_joint_2',
+                                           'right_rear_limb_joint_1',
+                                           'right_rear_limb_joint_2']))
         # symm_vel_pen = symm_rate*np.sum(np.abs(cur_dq['left_front_limb_joint_1', 'left_front_limb_joint_2'] - cur_dq[
         #     'right_front_limb_joint_1', 'right_front_limb_joint_2']))
         horizontal_pos_rwd = (cur_com[0] - old_com[0]) * 500
-        horizontal_vel_rwd = 0 #3*cur_dq[3]
-        orth_pen = 0.5* (np.abs(cur_com[1] - self.original_com[1]) + np.abs(cur_com[2] - self.original_com[2]))
+        horizontal_vel_rwd = 0  # 3*cur_dq[3]
+        orth_pen = 0.5 * (np.abs(cur_com[1] - self.original_com[1]) + np.abs(cur_com[2] - self.original_com[2]))
         rotate_pen = np.sum(np.abs(cur_q[:3] - self.original_q[:3]))
         # mirror_enforce
-        reward = 1 + horizontal_pos_rwd +  horizontal_vel_rwd - rotate_pen - orth_pen - symm_pos_pen
+        reward = 1 + horizontal_pos_rwd + horizontal_vel_rwd - rotate_pen - orth_pen - symm_pos_pen
 
         notdone = np.isfinite(ob[5::]).all() and (np.abs(angs) < np.pi / 2.0).all()
         done = not notdone
@@ -85,7 +86,8 @@ class DartTurtleSwimStraighSPDEnv(dart_env.DartEnv, utils.EzPickle):
                                   'rotate_pen': -rotate_pen, 'orth_pen': -orth_pen, 'symm_pos_pen': -symm_pos_pen}
 
     def _get_obs(self):
-        return np.concatenate([self.robot_skeleton.q[4:6],self.robot_skeleton.dq[3:6],self.robot_skeleton.q[6::], self.robot_skeleton.dq[6::]]).ravel()
+        return np.concatenate([self.robot_skeleton.q[4:6], self.robot_skeleton.dq[3:6], self.robot_skeleton.q[6::],
+                               self.robot_skeleton.dq[6::]]).ravel()
 
     def reset_model(self):
         self.dart_world.reset()
@@ -98,3 +100,40 @@ class DartTurtleSwimStraighSPDEnv(dart_env.DartEnv, utils.EzPickle):
         self._get_viewer().scene.tb.trans[2] = -3.5
         self._get_viewer().scene.tb._set_theta(-60)
         self.track_skeleton_id = 0
+
+class DartTurtleSwimStraighSPDEnvNoEnf(DartTurtleSwimStraighSPDEnv):
+
+    def _step(self, a):
+        old_com = self.robot_skeleton.C
+        old_q = self.robot_skeleton.q
+        old_dq = self.robot_skeleton.dq
+
+        target_pos = np.concatenate(([0.0] * 6, a * self.action_scale))
+
+        invM = self.invM
+        p = -self.Kp.dot(self.robot_skeleton.q + self.robot_skeleton.dq * self.simulation_dt - target_pos)
+        d = -self.Kd.dot(self.robot_skeleton.dq)
+        qddot = invM.dot(-self.robot_skeleton.c + p + d + self.robot_skeleton.constraint_forces())
+        tau = p + d - self.Kd.dot(qddot) * self.simulation_dt
+        tau *= 0.0005
+        tau[0:len(self.robot_skeleton.joints[0].dofs)] = 0
+        self.do_simulation(tau, self.frame_skip)
+        cur_com = self.robot_skeleton.C
+        cur_q = self.robot_skeleton.q
+        cur_dq = self.robot_skeleton.dq
+        ob = self._get_obs()
+
+        angs = np.abs(self.robot_skeleton.q[6::])
+        horizontal_pos_rwd = (cur_com[0] - old_com[0]) * 500
+        horizontal_vel_rwd = 0  # 3*cur_dq[3]
+        orth_pen = 0.5 * (np.abs(cur_com[1] - self.original_com[1]) + np.abs(cur_com[2] - self.original_com[2]))
+        rotate_pen = np.sum(np.abs(cur_q[:3] - self.original_q[:3]))
+        # mirror_enforce
+        reward = 1 + horizontal_pos_rwd + horizontal_vel_rwd - rotate_pen - orth_pen
+
+        notdone = np.isfinite(ob[5::]).all() and (np.abs(angs) < np.pi / 2.0).all()
+        done = not notdone
+
+        return ob, reward, done, {'rwd': reward, 'horizontal_pos_rwd': horizontal_pos_rwd,
+                                  'horizontal_vel_rwd': horizontal_vel_rwd,
+                                  'rotate_pen': -rotate_pen, 'orth_pen': -orth_pen}
