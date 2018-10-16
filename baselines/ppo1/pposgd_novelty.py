189064e551d90e116e9f77e6d3e58b934bee7f10
DragonMyth <tiger961212@gmail.com>
2018-10-16 17:25:49 -0400

    record task gradient mag

--- baselines/ppo1/pposgd_novelty.py
+++ baselines/ppo1/pposgd_novelty.py
@@ -272,6 +272,8 @@ def learn(env, policy_fn, *,
         assign_old_eq_new()  # set old parameter values to new parameter values
         logger.log("Optimizing...")
         logger.log(fmt_row(13, loss_names))
+        task_gradient_mag = []
+
         # Here we do a bunch of optimization epochs over the data
         for _ in range(optim_epochs):
             losses = []  # list of tuples, each of which gives the loss for a minibatch
@@ -281,6 +283,9 @@ def learn(env, policy_fn, *,
                 *newlosses_novel, g_novel = lossandgrad_novel(batch["ob"], batch["ac"], batch["atarg_novel"],
                                                               batch["vtarg_novel"],
                                                               cur_lrmult)
+
+                pol_g = g[0:policy_var_count]
+                task_gradient_mag.append(np.linalg.norm(pol_g))
 
                 adam.update(g, optim_stepsize * cur_lrmult)
 
@@ -321,6 +326,7 @@ def learn(env, policy_fn, *,
         logger.record_tabular("EpisodesSoFar", episodes_so_far)
         logger.record_tabular("TimestepsSoFar", timesteps_so_far)
         logger.record_tabular("TimeElapsed", time.time() - tstart)
+        logger.record_tabular("TaskGradMag", np.array(task_gradient_mag).mean())
         # logger.record_tabular("NoveltyUpdate", novelty_update)
         if MPI.COMM_WORLD.Get_rank() == 0:
             logger.dump_tabular()
