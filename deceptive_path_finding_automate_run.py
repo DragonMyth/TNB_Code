0e9440955da30ff597aa2d1e65d6c8fc38478d23
DragonMyth <tiger961212@gmail.com>
2018-12-10 16:47:51 -0500

    sync

--- deceptive_path_finding_automate_run.py
+++ deceptive_path_finding_automate_run.py
@@ -6,7 +6,7 @@ import multiprocessing
 from Util.post_training_process import *
 
 if __name__ == '__main__':
-    cpu_count = multiprocessing.cpu_count()
+    cpu_count = 8#multiprocessing.cpu_count()
     num_sample_per_iter = 12000
     num_trajs_per_pol = 200
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -40,85 +40,84 @@ if __name__ == '__main__':
     batch_size = 1024
     # qnorm = 10
     # dqnorm = 10
-    # for s in range(7):
-    s = 1
-    seed = s * 13 + 7 * (s ** 2)
+    for s in range(5):
+    #s = 1
+        seed = s * 13 + 7 * (s ** 2)
 
-    norm_scale = np.array([4, 10])
-    norm_scale_str = ''
-    for i in norm_scale:
-        norm_scale_str += str(i) + ' '
+        norm_scale = np.array([4, 10])
+        norm_scale_str = ''
+        for i in norm_scale:
+            norm_scale_str += str(i) + ' '
 
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
-    specified_time = None
-    for i in range(0, 6, 1):
-        # i = 0
-        curr_run = str(i)
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+        specified_time = None
+        for i in range(0, 5, 1):
+            # i = 0
+            curr_run = str(i)
 
-        # data_saving_path = 'data/ppo_' + env_name + '_seed_' + str(seed) + '_run_' + str(
-        #     curr_run) + '/' + '2018-10-01_16:53:21'
+            # data_saving_path = 'data/ppo_' + env_name + '_seed_' + str(seed) + '_run_' + str(
+            #     curr_run) + '/' + '2018-10-01_16:53:21'
 
-        if i == 0:
-            specified_time = None  # '2018-11-08_17:42:27'
-        else:
-            specified_time = None
+            if i == 0:
+                specified_time = None  # '2018-11-08_17:42:27'
+            else:
+                specified_time = None
 
-        if specified_time is None:
-            data_saving_path = 'data/local/' + str(st) + '_' + env_name + '/ppo_' + env_name + '_seed_' + str(
-                seed) + '_run_' + str(curr_run)
-            train_policy = subprocess.call(
-                'OMP_NUM_THREADS="1" mpirun -np ' + str(
-                    cpu_count) + ' python ./running_regimes/two_objs_policy_train.py'
-                + ' --env ' + args.env
+            if specified_time is None:
+                data_saving_path = 'data/local/' + str(st) + '_' + env_name + '_seed_' + str(
+                                        seed) + '/ppo_' + env_name + '_run_' + str(curr_run)
+                train_policy = subprocess.call(
+                    'OMP_NUM_THREADS="1" mpirun -np ' + str(
+                        cpu_count) + ' python ./running_regimes/two_objs_policy_train.py'
+                    + ' --env ' + args.env
+                    + ' --seed ' + str(seed)
+                    + ' --curr_run ' + curr_run
+                    + ' --data_saving_path ' + str(data_saving_path + '/policy')
+                    + ' --batch_size_per_process ' + str(args.batch_size_per_process)
+                    + ' --num_iterations ' + str(args.num_iterations)
+                    , shell=True)
+
+            else:
+                data_saving_path = 'data/local/' + str(specified_time) + '_' + env_name + '_seed_' + str(
+                                        seed) + '/ppo_' + env_name + '_run_' + str(curr_run)
+
+            collect_data = subprocess.call(
+                'OMP_NUM_THREADS="1" mpirun -np ' + str(cpu_count) + ' python ./Util/post_training_process.py'
                 + ' --seed ' + str(seed)
-                + ' --curr_run ' + curr_run
-                + ' --data_saving_path ' + str(data_saving_path + '/policy')
-                + ' --batch_size_per_process ' + str(args.batch_size_per_process)
-                + ' --num_iterations ' + str(args.num_iterations)
+                + ' --curr_run ' + str(curr_run)
+                + ' --data_collect_env ' + str(args.data_collect_env)
+                + ' --collect_policy_gap ' + str(args.collect_policy_gap)
+                + ' --collect_policy_num ' + str(args.collect_policy_num)
+                + ' --collect_policy_start ' + str(args.collect_policy_start)
+                + ' --collect_num_of_trajs ' + str(args.collect_num_of_trajs)
+                + ' --policy_saving_path ' + str(data_saving_path + '/policy')
+                + ' --ignore_obs ' + str(args.ignore_obs)
+                + ' --policy_fn_type ' + 'normal'
+                + ' --num_states_per_data ' + str(args.num_states_per_data)
+                + ' --obs_skip_per_state ' + str(args.obs_skip_per_state)
                 , shell=True)
+            #
+            collected_data_filename = 'novelty_data/local/sampled_paths/' + args.data_collect_env + '_seed_' + str(
+                seed) + '_run_' + str(
+                curr_run) + '.pkl'
 
-        else:
-            data_saving_path = 'data/local/' + str(
-                specified_time) + '_' + env_name + '/ppo_' + env_name + '_seed_' + str(
-                seed) + '_run_' + str(curr_run)
+            plot_save_dir = 'novelty_data/local/sampled_paths/plots/' + args.data_collect_env + '_seed_' + str(
+                seed) + '_run_' + str(
+                curr_run) + '_visited_plot.png'
 
-        collect_data = subprocess.call(
-            'OMP_NUM_THREADS="1" mpirun -np ' + str(cpu_count) + ' python ./Util/post_training_process.py'
-            + ' --seed ' + str(seed)
-            + ' --curr_run ' + str(curr_run)
-            + ' --data_collect_env ' + str(args.data_collect_env)
-            + ' --collect_policy_gap ' + str(args.collect_policy_gap)
-            + ' --collect_policy_num ' + str(args.collect_policy_num)
-            + ' --collect_policy_start ' + str(args.collect_policy_start)
-            + ' --collect_num_of_trajs ' + str(args.collect_num_of_trajs)
-            + ' --policy_saving_path ' + str(data_saving_path + '/policy')
-            + ' --ignore_obs ' + str(args.ignore_obs)
-            + ' --policy_fn_type ' + 'normal'
-            + ' --num_states_per_data ' + str(args.num_states_per_data)
-            + ' --obs_skip_per_state ' + str(args.obs_skip_per_state)
-            , shell=True)
-        #
-        collected_data_filename = 'novelty_data/local/sampled_paths/' + args.data_collect_env + '_seed_' + str(
-            seed) + '_run_' + str(
-            curr_run) + '.pkl'
+            plot_path_data(collected_data_filename, plot_save_dir)
 
-        plot_save_dir = 'novelty_data/local/sampled_paths/plots/' + args.data_collect_env + '_seed_' + str(
-            seed) + '_run_' + str(
-            curr_run) + '_visited_plot.png'
+            train_autoencoder = subprocess.call('OMP_NUM_THREADS="1" python ./Util/train_traj_autoencoder.py'
+                                                + ' --env ' + str(args.env)
+                                                + ' --seed ' + str(seed)
+                                                + ' --data_collect_env ' + str(args.data_collect_env)
+                                                + ' --curr_run ' + str(curr_run)
+                                                # + ' --qnorm ' + str(qnorm)
+                                                # + ' --dqnorm ' + str(dqnorm)
+                                                + ' --num_epoch ' + str(num_epoch)
+                                                + ' --batch_size ' + str(batch_size)
+                                                + ' --norm_scales ' + str(norm_scale_str)
+                                                , shell=True
 
-        plot_path_data(collected_data_filename, plot_save_dir)
-
-        train_autoencoder = subprocess.call('OMP_NUM_THREADS="1" python ./Util/train_traj_autoencoder.py'
-                                            + ' --env ' + str(args.env)
-                                            + ' --seed ' + str(seed)
-                                            + ' --data_collect_env ' + str(args.data_collect_env)
-                                            + ' --curr_run ' + str(curr_run)
-                                            # + ' --qnorm ' + str(qnorm)
-                                            # + ' --dqnorm ' + str(dqnorm)
-                                            + ' --num_epoch ' + str(num_epoch)
-                                            + ' --batch_size ' + str(batch_size)
-                                            + ' --norm_scales ' + str(norm_scale_str)
-                                            , shell=True
-
-                                            )
+                                                )
--- turtle_more_info_forward_automate_run.py
+++ turtle_more_info_forward_automate_run.py
@@ -6,7 +6,7 @@ import multiprocessing
 from Util.post_training_process import *
 
 if __name__ == '__main__':
-    cpu_count = 12#multiprocessing.cpu_count()
+    cpu_count = 8#multiprocessing.cpu_count()
     num_sample_per_iter = 12000
     print("Number of processes: ", cpu_count)
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -35,71 +35,70 @@ if __name__ == '__main__':
     batch_size = 1024
     # qnorm = np.pi
     # dqnorm = 50
-    # for s in range(7):
-    #     seed = s * 13 + 7 * (s ** 2)
+    for s in range(5):
+        seed = s * 13 + 7 * (s ** 2)
 
-    norm_scale = np.array([8, 2 * np.pi, 8, 50])
-    norm_scale_str = ''
-    for i in norm_scale:
-        norm_scale_str += str(i) + ' '
+        norm_scale = np.array([8, 2 * np.pi, 8, 50])
+        norm_scale_str = ''
+        for i in norm_scale:
+            norm_scale_str += str(i) + ' '
 
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
 
-    for i in range(0, 6, 1):
-        # i = 0
-        curr_run = str(i)
-        # data_saving_path = 'data/ppo_' + env_name + '_seed_' + str(seed) + '_run_' + str(
-        #     curr_run) + '/' + '2018-10-01_16:53:21'
-        data_saving_path = 'data/local/' + str(st) + '_' + env_name + '/ppo_' + env_name + '_seed_' + str(
-            seed) + '_run_' + str(curr_run)
+        for i in range(0, 5, 1):
+            # i = 0
+            curr_run = str(i)
+            # data_saving_path = 'data/ppo_' + env_name + '_seed_' + str(seed) + '_run_' + str(
+            #     curr_run) + '/' + '2018-10-01_16:53:21'
+            data_saving_path = 'data/local/' + str(st) + '_' + env_name + '_seed_' + str(
+                                    seed) + '/ppo_' + env_name + '_run_' + str(curr_run)
+            train_policy = subprocess.call(
+                'OMP_NUM_THREADS="1" mpirun -np ' + str(
+                    cpu_count) + ' python ./running_regimes/turtle_more_info_two_objs_mirror_policy_train.py'
+                + ' --env ' + args.env
+                + ' --seed ' + str(seed)
+                + ' --curr_run ' + curr_run
+                + ' --data_saving_path ' + data_saving_path + '/policy'
+                + ' --batch_size_per_process ' + str(args.batch_size_per_process)
+                + ' --num_iterations ' + str(args.num_iterations)
+                , shell=True)
 
-        train_policy = subprocess.call(
-            'OMP_NUM_THREADS="1" mpirun -np ' + str(
-                cpu_count) + ' python ./running_regimes/turtle_more_info_two_objs_mirror_policy_train.py'
-            + ' --env ' + args.env
-            + ' --seed ' + str(seed)
-            + ' --curr_run ' + curr_run
-            + ' --data_saving_path ' + data_saving_path + '/policy'
-            + ' --batch_size_per_process ' + str(args.batch_size_per_process)
-            + ' --num_iterations ' + str(args.num_iterations)
-            , shell=True)
+            collect_data = subprocess.call(
+                'OMP_NUM_THREADS="1" mpirun -np ' + str(cpu_count) + ' python ./Util/post_training_process.py'
+                + ' --seed ' + str(seed)
+                + ' --curr_run ' + str(curr_run)
+                + ' --data_collect_env ' + str(args.data_collect_env)
+                + ' --collect_policy_gap ' + str(args.collect_policy_gap)
+                + ' --collect_policy_num ' + str(args.collect_policy_num)
+                + ' --collect_policy_start ' + str(args.collect_policy_start)
+                + ' --collect_num_of_trajs ' + str(args.collect_num_of_trajs)
+                + ' --policy_saving_path ' + str(data_saving_path + '/policy')
+                + ' --ignore_obs ' + str(args.ignore_obs)
+                + ' --policy_fn_type ' + 'turtle'
+                , shell=True)
+            #
+            collected_data_filename = 'novelty_data/local/sampled_paths/' + args.data_collect_env + '_seed_' + str(
+                seed) + '_run_' + str(
+                curr_run) + '.pkl'
 
-        collect_data = subprocess.call(
-            'OMP_NUM_THREADS="1" mpirun -np ' + str(cpu_count) + ' python ./Util/post_training_process.py'
-            + ' --seed ' + str(seed)
-            + ' --curr_run ' + str(curr_run)
-            + ' --data_collect_env ' + str(args.data_collect_env)
-            + ' --collect_policy_gap ' + str(args.collect_policy_gap)
-            + ' --collect_policy_num ' + str(args.collect_policy_num)
-            + ' --collect_policy_start ' + str(args.collect_policy_start)
-            + ' --collect_num_of_trajs ' + str(args.collect_num_of_trajs)
-            + ' --policy_saving_path ' + str(data_saving_path + '/policy')
-            + ' --ignore_obs ' + str(args.ignore_obs)
-            + ' --policy_fn_type ' + 'turtle'
-            , shell=True)
-        #
-        collected_data_filename = 'novelty_data/local/sampled_paths/' + args.data_collect_env + '_seed_' + str(
-            seed) + '_run_' + str(
-            curr_run) + '.pkl'
+            plot_save_dir = 'novelty_data/local/sampled_paths/plots/' + args.data_collect_env + '_seed_' + str(
+                seed) + '_run_' + str(
+                curr_run) + '_visited_plot.png'
 
-        plot_save_dir = 'novelty_data/local/sampled_paths/plots/' + args.data_collect_env + '_seed_' + str(
-            seed) + '_run_' + str(
-            curr_run) + '_visited_plot.png'
+            plot_path_data(collected_data_filename, plot_save_dir)
 
-        plot_path_data(collected_data_filename, plot_save_dir)
+            train_autoencoder = subprocess.call('OMP_NUM_THREADS="1" python ./Util/train_traj_autoencoder.py'
+                                                + ' --env ' + str(args.env)
+                                                + ' --seed ' + str(seed)
+                                                + ' --data_collect_env ' + str(args.data_collect_env)
+                                                + ' --curr_run ' + str(curr_run)
+                                                # + ' --qnorm ' + str(qnorm)
+                                                # + ' --dqnorm ' + str(dqnorm)
+                                                + ' --num_epoch ' + str(num_epoch)
+                                                + ' --batch_size ' + str(batch_size)
+                                                + ' --norm_scales ' + str(norm_scale_str)
 
-        train_autoencoder = subprocess.call('OMP_NUM_THREADS="1" python ./Util/train_traj_autoencoder.py'
-                                            + ' --env ' + str(args.env)
-                                            + ' --seed ' + str(seed)
-                                            + ' --data_collect_env ' + str(args.data_collect_env)
-                                            + ' --curr_run ' + str(curr_run)
-                                            # + ' --qnorm ' + str(qnorm)
-                                            # + ' --dqnorm ' + str(dqnorm)
-                                            + ' --num_epoch ' + str(num_epoch)
-                                            + ' --batch_size ' + str(batch_size)
-                                            + ' --norm_scales ' + str(norm_scale_str)
+                                                , shell=True
 
-                                            , shell=True
-
-                                            )
+                                                )
