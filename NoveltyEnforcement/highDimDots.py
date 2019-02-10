36edd15e04be7912ddb2cfb522f7c5aa9497cc21
DragonMyth <tiger961212@gmail.com>
2019-02-10 14:52:52 -0500

    rearranged code

--- NoveltyEnforcement/highDimDots.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-sns.set()
-
-current_palette = sns.color_palette()
-sns.palplot(current_palette)
-print(current_palette)
