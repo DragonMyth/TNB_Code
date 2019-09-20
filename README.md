210b7060cb86773823578066c24f5f6b3f697790
yunbozhangGT <ybzhang3027@gatech.edu>
2019-09-20 13:02:12 -0400

    cleaned

---
+++ README.md
@@ -0,0 +1,33 @@
+# Learning Novel Policies For Tasks
+
+Yunbo Zhang, Wenhao Yu, Greg Turk
+
+#### ICML 2019  
+[[website]](https://sites.google.com/view/learningnovelpolicy/home) 
+[[Paper]](https://arxiv.org/pdf/1905.05252.pdf)
+
+## Dependencies
+This implementation is built on top of the following repositories:
+
+[OpenAi Gym](https://github.com/openai/gym)
+
+[DartEnv](https://github.com/DartEnv/dart-env)
+
+[OpenAI Baselines](https://github.com/openai/baselines)
+
+## Installation
+After Installation of all repos,  copy 
+`baselines/baselines/ppo1` and `dart-env/gym/envs`  to corresponding installed locations. 
+
+## Demo Training
+### Four Way Maze
+run `python simple_path_finding_automate_run.py`
+### 3d-Reacher
+run `python dart_3d_reacher_automate_run.py`
+### Deceptive Reacher
+run `python dart_3d_reacher_deceptive_automate_run.py`
+### Hopper
+run `python dart_hopper_automate_run.py`
+### Deceptive Maze
+run `python deveptive_path_finding_automate_run.py`
+
--- TNB/visualize_results.py
+++ TNB/visualize_results.py
@@ -1,4 +1,4 @@
-from Util.post_training_process import *
+from TNB.Util.post_training_process import *
 
 
 #
@@ -39,38 +39,14 @@ def render_deceptive_reacher(random=False):
                   autoencoder_name_list=[], random_policy=random, num_runs=1)
 
 
-def render_turtles():
-    autoencoder_dir = "novelty_data/rome/autoencoders/"
-
-    autoencoder0_name = autoencoder_dir + "DartTurtle-v4_autoencoder_seed_0_run_0.h5"
-    # autoencoder1_name = autoencoder_dir + "DartTurtle-v4_autoencoder_seed_0_run_1.h5"
-    # autoencoder3 = load_model(autoencoder_dir + "SimplerPa thFinding-v0_autoencoder_for_run_2_seed_102.h5")
-    # autoencoder4 = load_model(autoencoder_dir + "new_path_finding_autoencoder_autoencoder_4_seed=54.h5")
-    autoencoder_list = [autoencoder0_name]
-    render_policy('DartTurtle-v4', stoch=False, record=True, policy_func=mirror_turtle_policy_fn,
-                  autoencoder_name_list=autoencoder_list, random_policy=False)
-
-
 def render_deceptive_maze():
-    autoencoder_list = []
     render_policy('PathFindingDeceptive-v0', stoch=True, record=True,
                   autoencoder_name_list=[], num_runs=4)
 
 
 def render_four_way_maze():
-    autoencoder_list = []
     render_policy('SimplerPathFinding-v0', stoch=True, record=True,
                   autoencoder_name_list=[], num_runs=1)
-
-
-def render_humanoid():
-    render_policy('DartHumanSwim-v0', stoch=False, record=True, policy_func=mirror_humanoid_policy_fn,
-                  autoencoder_name_list=[], random_policy=False)
-
-
-def render_point_mass_wandering():
-    render_policy('PointMassWandering-v0', stoch=False, record=False,
-                  autoencoder_name_list=[], random_policy=False)
 
 
 def render_hopper():
@@ -78,21 +54,4 @@ def render_hopper():
                   autoencoder_name_list=[], random_policy=False)
 
 
-def render_2d_walker():
-    render_policy('DartWalker2d-v1', stoch=False, record=True,
-                  autoencoder_name_list=[], random_policy=False)
-
-
-# render_four_way_maze()
-# render_point_mass_wandering()
-
-# render_2d_walker()
-
-# render_turtles()
-# render_humanoid()
-# render_deceptive_maze()
 render_deceptive_reacher()
-# render_reacher()
-# render_hopper()
-
-# plot_path_data()
